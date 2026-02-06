b7e7ae28e92d4e5425335a402cffbce16e28b127
Omar alhallak <omaralhallak0998976714@gmail.com>
2026-02-07 02:31:12 +0300

    Fiixed

---
+++ calculator/Core/Operations/OperatorHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator.Core.Operations
+{
+    public class OperatorHelper
+    {
+        public static bool isOperator(char ch)
+        {
+            if (ch == '+' || ch == '-' || ch == '×' || ch == '÷')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
